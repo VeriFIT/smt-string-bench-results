daa5051f0b760723454fb939d28605b9d12a36b9
jurajsic <juraj.sic@outlook.com>
2023-04-04 09:31:30 +0200

    sort now properly

--- get_tasks_and_generate_csv.sh
+++ get_tasks_and_generate_csv.sh
@@ -7,6 +7,6 @@ GIT_COMMIT=$(ggrep -m 1 -Po '.{7}(?=-result)' $path_to_file)
 sed -i '' "s/$GIT_COMMIT-result/result/g" $path_to_file
 sed -i '' "s/z3-noodler/z3-noodler-$GIT_COMMIT/g" $path_to_file
 
-ls $benchmark_name/*.tasks | sed "s/-underapprox/underapprox/g" | sort -t- -k 5 | sed "s/underapprox/-underapprox/g" | python3 pyco_proc --csv > $benchmark_name/to120.csv
+ls $benchmark_name/*.tasks | sed "s/-underapprox/underapprox/g" | sort -t- -k 5 | sed "s/underapprox/-underapprox/g" | xargs cat | python3 pyco_proc --csv > $benchmark_name/to120.csv
 git add $path_to_file $benchmark_name/to120.csv
 git commit -m "z3-noodler-$GIT_COMMIT on $benchmark_name"
